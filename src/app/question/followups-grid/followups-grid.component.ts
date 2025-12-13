65740cabd87128d9fc1b0a6f76d946ba906c70a8
damir <damir.magamedov@gmail.com>
2025-12-13 14:45:04 +0200

    refactor: Simplifies follow-ups grid layout by removing grid list and enhancing card display for better responsiveness

--- src/app/question/followups-grid/followups-grid.component.ts
+++ src/app/question/followups-grid/followups-grid.component.ts
@@ -1,10 +1,8 @@
-import { Component, EventEmitter, Input, Output, OnInit, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { IQuestion } from '../../core/models';
 
 @Component({
@@ -15,32 +13,14 @@ import { IQuestion } from '../../core/models';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatGridListModule,
   ],
   templateUrl: './followups-grid.component.html',
   styleUrls: ['./followups-grid.component.scss'],
 })
-export class FollowupsGridComponent implements OnInit {
+export class FollowupsGridComponent {
   @Input() followUps: IQuestion[] = [];
   @Input() parentQuestionId: string | null = null;
   @Output() followUpSelected = new EventEmitter<IQuestion>();
-
-  gridCols = 3;
-  private readonly breakpointObserver = inject(BreakpointObserver);
-
-  ngOnInit() {
-    this.breakpointObserver
-      .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium])
-      .subscribe((state) => {
-        if (state.breakpoints[Breakpoints.XSmall] || state.breakpoints[Breakpoints.Small]) {
-          this.gridCols = 1;
-        } else if (state.breakpoints[Breakpoints.Medium]) {
-          this.gridCols = 2;
-        } else {
-          this.gridCols = 3;
-        }
-      });
-  }
 
   selectFollowUp(followUp: IQuestion) {
     this.followUpSelected.emit(followUp);
